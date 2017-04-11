8001c0ef8303876fa7626574bb798eed681cb432
Dennis Schwerdel <schwerdel@informatik.uni-kl.de>
2017-04-11 09:08:24 +0200

    Updated comparison

--- test.sh
+++ test.sh
@@ -1,21 +1,22 @@
 set -ex
 
+rm -rf repos
 mkdir repos
-time target/release/zvault init --compression brotli/3 repos/zvault_brotli3
-time target/release/zvault init --compression brotli/6 repos/zvault_brotli6
-time target/release/zvault init --compression lzma2/2 repos/zvault_lzma2
-time attic init repos/attic
-time borg init -e none repos/borg
-time borg init -e none repos/borg-zlib
-time zbackup init --non-encrypted repos/zbackup
+target/release/zvault init --compression brotli/3 repos/zvault_brotli3
+target/release/zvault init --compression brotli/6 repos/zvault_brotli6
+target/release/zvault init --compression lzma2/2 repos/zvault_lzma2
+attic init repos/attic
+borg init -e none repos/borg
+borg init -e none repos/borg-zlib
+zbackup init --non-encrypted repos/zbackup
 
 cat < test_data/silesia.tar > /dev/null
-time target/release/zvault put repos/zvault_brotli3::silesia1 test_data/silesia.tar
-time target/release/zvault put repos/zvault_brotli3::silesia2 test_data/silesia.tar
-time target/release/zvault put repos/zvault_brotli6::silesia1 test_data/silesia.tar
-time target/release/zvault put repos/zvault_brotli6::silesia2 test_data/silesia.tar
-time target/release/zvault put repos/zvault_lzma2::silesia1 test_data/silesia.tar
-time target/release/zvault put repos/zvault_lzma2::silesia2 test_data/silesia.tar
+time target/release/zvault backup repos/zvault_brotli3::silesia1 test_data/silesia.tar
+time target/release/zvault backup repos/zvault_brotli3::silesia2 test_data/silesia.tar
+time target/release/zvault backup repos/zvault_brotli6::silesia1 test_data/silesia.tar
+time target/release/zvault backup repos/zvault_brotli6::silesia2 test_data/silesia.tar
+time target/release/zvault backup repos/zvault_lzma2::silesia1 test_data/silesia.tar
+time target/release/zvault backup repos/zvault_lzma2::silesia2 test_data/silesia.tar
 time attic create repos/attic::silesia1 test_data/silesia.tar
 time attic create repos/attic::silesia2 test_data/silesia.tar
 time borg create -C none repos/borg::silesia1 test_data/silesia.tar
@@ -29,24 +30,22 @@ du -h test_data/silesia.tar
 du -sh repos/zvault*/bundles repos/attic repos/borg repos/borg-zlib repos/zbackup
 
 rm -rf repos
-
-
 mkdir repos
-time target/release/zvault init --compression brotli/3 repos/zvault_brotli3
-time target/release/zvault init --compression brotli/6 repos/zvault_brotli6
-time target/release/zvault init --compression lzma2/2 repos/zvault_lzma2
-time attic init repos/attic
-time borg init -e none repos/borg
-time borg init -e none repos/borg-zlib
-time zbackup init --non-encrypted repos/zbackup
+target/release/zvault init --compression brotli/3 repos/zvault_brotli3
+target/release/zvault init --compression brotli/6 repos/zvault_brotli6
+target/release/zvault init --compression lzma2/2 repos/zvault_lzma2
+attic init repos/attic
+borg init -e none repos/borg
+borg init -e none repos/borg-zlib
+zbackup init --non-encrypted repos/zbackup
 
 cat < test_data/ubuntu.tar > /dev/null
-time target/release/zvault put repos/zvault_brotli3::ubuntu1 test_data/ubuntu.tar
-time target/release/zvault put repos/zvault_brotli3::ubuntu2 test_data/ubuntu.tar
-time target/release/zvault put repos/zvault_brotli6::ubuntu1 test_data/ubuntu.tar
-time target/release/zvault put repos/zvault_brotli6::ubuntu2 test_data/ubuntu.tar
-time target/release/zvault put repos/zvault_lzma2::ubuntu1 test_data/ubuntu.tar
-time target/release/zvault put repos/zvault_lzma2::ubuntu2 test_data/ubuntu.tar
+time target/release/zvault backup repos/zvault_brotli3::ubuntu1 test_data/ubuntu.tar
+time target/release/zvault backup repos/zvault_brotli3::ubuntu2 test_data/ubuntu.tar
+time target/release/zvault backup repos/zvault_brotli6::ubuntu1 test_data/ubuntu.tar
+time target/release/zvault backup repos/zvault_brotli6::ubuntu2 test_data/ubuntu.tar
+time target/release/zvault backup repos/zvault_lzma2::ubuntu1 test_data/ubuntu.tar
+time target/release/zvault backup repos/zvault_lzma2::ubuntu2 test_data/ubuntu.tar
 time attic create repos/attic::ubuntu1 test_data/ubuntu.tar
 time attic create repos/attic::ubuntu2 test_data/ubuntu.tar
 time borg create -C none repos/borg::ubuntu1 test_data/ubuntu.tar
